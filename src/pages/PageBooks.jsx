b97643d1b7900d9f7fb44a55da0c41cf80b8b9a5
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-05-29 15:35:54 +0200

    fetch techBooks

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
+
 export const PageBooks = () => {
+  const url = 'https://edwardtanguay.netlify.app/share/techBooks.json';
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchTechBooks = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setBooks(data);
+    };
+
+    fetchTechBooks();
+  }, []);
+
   return (
     <div className='books'>
       <h2>Books</h2>
-      <p>Here are th books: </p>
+      <p>Here one book picture: </p>
       <img src='images/bashCookbook.jpg' alt='bashbook cover' />
+
+      <div className='techBooks'>
+        <p>Also fetch books from ohter sites: </p>
+        <ul>
+          {books.map((book) => {
+            return <li key={book.id}> {book.title}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
