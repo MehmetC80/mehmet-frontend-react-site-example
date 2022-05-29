e035d7f284ddb22d90863219df6e02b560b921f5
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-05-29 11:48:01 +0200

    create public/images folder and add image

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div className='App'>
       <h1>Frontend-React-Site-Example</h1>
       <p>Welcome to this site.</p>
+      <img src='images/bashCookbook.jpg' alt='bashbook cover' />
     </div>
   );
 }
